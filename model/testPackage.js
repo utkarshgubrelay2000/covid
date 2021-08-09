c15dd75a3517bd4f9bacb02d3f1fb1436be928d1
Utkarsh Gubrelay <utkarshgubrelay2000@gmail.com>
2021-08-09 17:35:47 +0530

    book

---
+++ model/testPackage.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Test = require("./testModel");
+
+const testPackageSchema = new mongoose.Schema(
+  {
+    testId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Test",
+    },
+    packageName: {
+      type: String,
+      required: true,
+    },
+    packageTitle: {
+      type: String,
+    },
+    packageDescription: {
+      type: String,
+    },
+    subPackageName: {
+      type: String,
+    },
+    mandatoryDays: {
+      type: Number,
+    },
+    testResult: {
+      type: String,
+    },
+    price1: {
+      type: Number,
+    },
+    subPackageNameHome: {
+      type: String,
+    },
+    testResultHome: {
+      type: Number,
+    },
+    price2: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const TestPackage = new mongoose.model("TestPackage", testPackageSchema);
+
+module.exports = TestPackage;
