acc50bde59aebfa43760458181b96c38438d7430
Utkarsh Gubrelay <utkarshgubrelay2000@gmail.com>
2021-08-17 13:12:10 +0530

    approved

--- controller/routerController.js
+++ controller/routerController.js
@@ -1,4 +1,3 @@
-
 const FAQ = require("../model/faqModel");
 const Test = require("../model/testModel");
 let { Scheduler } = require("@ssense/sscheduler");
@@ -22,10 +21,10 @@ exports.homePage = (req, res) => {
 exports.contact = (req, res) => {
   res.render("contact");
 };
-exports.about =async (req, res) => {
-  let  data = await aboutmodel.find({})
- // console.log(data)
-    res.render("about-us",{data:data[0]});
+exports.about = async (req, res) => {
+  let data = await aboutmodel.find({});
+  // console.log(data)
+  res.render("about-us", { data: data[0] });
 };
 
 exports.faq = async (req, res) => {
@@ -44,61 +43,104 @@ exports.loadMoreFaqs = async (req, res) => {
     res.send("Error");
   }
 };
-exports.appointment =async (req, res) => {
-  let userDetails= await User.findById(req.body.userId,{password:0,gender:0,createdAt:0,updatedAt:0})
-let myslots= await  TimeSlots.find({UserId:req.body.userId,approved:false}).populate("test")
-.populate("packageid")
-.populate("user").limit(10);
-let completeslots= await  TimeSlots.find({UserId:req.body.userId,approved:true})
-.populate("test")
-.populate("packageid")
-.populate("user").limit(10);
-//console.log(myslots[0],req.body)
-  res.render("appointment-bookings",{myslots:myslots,data:userDetails,token:req.params.token,curpage:1,completeslots:completeslots});
+exports.appointment = async (req, res) => {
+  let userDetails = await User.findById(req.body.userId, {
+    password: 0,
+    gender: 0,
+    createdAt: 0,
+    updatedAt: 0,
+  });
+  let myslots = await TimeSlots.find({
+    UserId: req.body.userId,
+    booked: true,
+    barcode: { $exists: false },
+  })
+    .populate("test")
+    .populate("packageid")
+    .populate("user")
+    .limit(10);
+  let completeslots = await TimeSlots.find({
+    UserId: req.body.userId,
+    approved: true,
+  })
+    .populate("test")
+    .populate("packageid")
+    .populate("user")
+    .limit(10);
+  let reports = await TimeSlots.find({
+    booked: true,
+    barcode: { $exists: true },
+  })
+    .populate("User")
+    .populate("test")
+    .populate("packageid")
+    .populate("user")
+    .limit(10);
+  //console.log(myslots[0],req.body)
+  res.render("appointment-bookings", {
+    myslots: myslots,
+    data: userDetails,
+    token: req.params.token,
+    curpage: 1,
+    completeslots: completeslots,
+    reports: reports,
+  });
 };
-exports.paginationappointment =async (req, res) => {
-  let myslots= await  TimeSlots.find({UserId:req.body.userId}).populate("UserId")
-  .populate("test")
-  .populate("packageid")
-  .populate("user").limit(10).skip(req.body.curpage*10);
-  console.log(myslots[0].user,req.body)
-    res.render("appointment-bookings",{myslots:myslots,token:req.params.token,curpage:req.body.curpage+1});
-  };
+exports.paginationappointment = async (req, res) => {
+  let myslots = await TimeSlots.find({ UserId: req.body.userId })
+    .populate("UserId")
+    .populate("test")
+    .populate("packageid")
+    .populate("user")
+    .limit(10)
+    .skip(req.body.curpage * 10);
+  console.log(myslots[0].user, req.body);
+  res.render("appointment-bookings", {
+    myslots: myslots,
+    token: req.params.token,
+    curpage: req.body.curpage + 1,
+  });
+};
 exports.chooseslots = async (req, res) => {
   function addDays(theDate, days) {
     return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
   }
   // let today = new Date()
-  console.log(req.params)
+  console.log(req.params);
   let newDate = new Date();
   let nextDate = addDays(newDate, 1);
 
-  const tests = await Test.findOne({test_name:req.params.name});
+  const tests = await Test.findOne({ test_name: req.params.name });
   const slots = await TimeSlots.find({
     test: req.params.id,
     booked: false,
     bookedFor: { $gt: newDate, $lt: nextDate },
   });
-if(!tests.date){
-  res.render("choose-slots", { _id: req.params.name, slots: slots,testDetails:tests,pachageId:req.params.packId });
-}
-else{
-  res.render("choose-slots-days", { _id: req.params.id, slots: slots,testDetails:tests,pachageId:req.params.packId });
-
-}
+  if (!tests.date) {
+    res.render("choose-slots", {
+      _id: req.params.name,
+      slots: slots,
+      testDetails: tests,
+      pachageId: req.params.packId,
+    });
+  } else {
+    res.render("choose-slots-days", {
+      _id: req.params.id,
+      slots: slots,
+      testDetails: tests,
+      pachageId: req.params.packId,
+    });
+  }
 };
 exports.contactdetails = async (req, res) => {
-let {spots,people,packageid}=req.body
+  let { spots, people, packageid } = req.body;
   let test = await Test.findOne({ _id: req.body._id });
   let peopleArray = [];
   let peoplesDetails = {
     email: "",
-  
-    
   };
-  if(people==1){
-    spots=[spots]
-  
+  if (people == 1) {
+    spots = [spots];
   }
   for (let index = 0; index < Number(req.body.people); index++) {
     peopleArray.push(peoplesDetails);
@@ -113,24 +155,22 @@ let {spots,people,packageid}=req.body
     length: peopleArray.length,
   });
 };
-exports.contactdetailsforArrivinginEngland=async (req, res) => {
- let {spotArrival,spotAfterDay6,date,people,packageid}=req.body
- let test = await TestPackage.findOne({ _id: req.body.packageid });
+exports.contactdetailsforArrivinginEngland = async (req, res) => {
+  let { spotArrival, spotAfterDay6, date, people, packageid } = req.body;
+  let test = await TestPackage.findOne({ _id: req.body.packageid });
   let peopleArray = [];
   let peoplesDetails = {
     email: "",
   };
- 
-     
-  
-  if(people==1){
-    spotAfterDay6=[spotAfterDay6]
-    spotArrival=[spotArrival]
+
+  if (people == 1) {
+    spotAfterDay6 = [spotAfterDay6];
+    spotArrival = [spotArrival];
   }
   for (let index = 0; index < Number(req.body.people); index++) {
     peopleArray.push(peoplesDetails);
   }
-  console.log(spotArrival,spotAfterDay6);
+  console.log(spotArrival, spotAfterDay6);
   res.render("contact-for-arrival", {
     testDetails: test,
     packageid: packageid,
@@ -140,7 +180,7 @@ exports.contactdetailsforArrivinginEngland=async (req, res) => {
     people: peopleArray,
     length: people,
   });
-}
+};
 exports.contact = (req, res) => {
   res.render("contact");
 };
@@ -154,12 +194,18 @@ exports.notification = (req, res) => {
   res.render("notification");
 };
 exports.profile = (req, res) => {
-  User.findById(req.body.userId,{password:0,gender:0,createdAt:0,updatedAt:0}).then(found=>{
-   
-    res.render("profile",{data:found,token:req.params.token});
-  }).catch((err) => {
-    res.send({msg: "Something Went Wrong " });
-  });
+  User.findById(req.body.userId, {
+    password: 0,
+    gender: 0,
+    createdAt: 0,
+    updatedAt: 0,
+  })
+    .then((found) => {
+      res.render("profile", { data: found, token: req.params.token });
+    })
+    .catch((err) => {
+      res.send({ msg: "Something Went Wrong " });
+    });
 };
 exports.settings = (req, res) => {
   res.render("settings");
@@ -173,23 +219,22 @@ exports.testTerms = (req, res) => {
 
 exports.testslisting = async (req, res) => {
   let tests = await Test.find({});
-  
+
   res.render("tests-listing", { tests: tests });
 };
 exports.testsbyId = async (req, res) => {
-  console.log(req.params)
+  console.log(req.params);
   let tests = await TestPackage.find({ testId: req.params.id });
-  res.send({ tests: tests,testId:req.params.id ,name:req.params.name});
+  res.send({ tests: tests, testId: req.params.id, name: req.params.name });
 };
 exports.createBooking = async (req, res) => {
   try {
-    const { slots,packageid } = req.body;
- let   sloted=slots.split(',')
-   // console.log('hello',sloted)
-      personal_details=JSON.parse(req.body.personal_details)
-     
+    const { slots, packageid } = req.body;
+    let sloted = slots.split(",");
+    // console.log('hello',sloted)
+    personal_details = JSON.parse(req.body.personal_details);
+
     const validation = validate(req.body, {
-     
       slots: {
         presence: true,
       },
@@ -205,39 +250,38 @@ exports.createBooking = async (req, res) => {
       });
       return console.log(validation);
     }
-    console.log('here')
-    let users=[]
-     const promises =   personal_details.map((person, index) => {
+    console.log("here");
+    let users = [];
+    const promises = personal_details.map((person, index) => {
       const details = new PersonalDetails({ ...person, slot: sloted[index] });
-     details.save().then(async saved=>{
-       users.push(saved._id)
-     let res=await TimeSlots.findOneAndUpdate(
-        { _id: sloted[index] },
-        { booked: true, user: saved._id,UserId:req.body.userId, ...req.body,packageid:packageid  },
-        { new: true }
-      );
-      console.log(res)
-     return saved._id
-    })
-    });
- 
- 
-   
-    setTimeout(() => {
-      
- 
-    console.log(users,promises)
-    if (promises)
-      res
-        .json({ message: "Booking Saved!",users:users , status: true });
-    else
-      res.status(400).json({
-        status: false,
-        data: 'Something wrong',
+      details.save().then(async (saved) => {
+        users.push(saved._id);
+        let res = await TimeSlots.findOneAndUpdate(
+          { _id: sloted[index] },
+          {
+            booked: true,
+            user: saved._id,
+            UserId: req.body.userId,
+            ...req.body,
+            packageid: packageid,
+          },
+          { new: true }
+        );
+        console.log(res);
+        return saved._id;
       });
-    }, 600);
+    });
 
-   
+    setTimeout(() => {
+      console.log(users, promises);
+      if (promises)
+        res.json({ message: "Booking Saved!", users: users, status: true });
+      else
+        res.status(400).json({
+          status: false,
+          data: "Something wrong",
+        });
+    }, 600);
   } catch (error) {
     res.status(503).json({
       status: false,
@@ -247,14 +291,13 @@ exports.createBooking = async (req, res) => {
 };
 exports.createBookingArrival = async (req, res) => {
   try {
-    const { slots,slotAfterDay6,packageid } = req.body;
-    let sloted=slots.split(',')
-    let slotedDay6=slotAfterDay6.split(',')
-    personal_details=JSON.parse(req.body.personal_details)
-    console.log('hello',sloted,personal_details)
-     
+    const { slots, slotAfterDay6, packageid } = req.body;
+    let sloted = slots.split(",");
+    let slotedDay6 = slotAfterDay6.split(",");
+    personal_details = JSON.parse(req.body.personal_details);
+    console.log("hello", sloted, personal_details);
+
     const validation = validate(req.body, {
-     
       slots: {
         presence: true,
       },
@@ -270,36 +313,46 @@ exports.createBookingArrival = async (req, res) => {
       });
       return console.log(validation);
     }
-    console.log('here',personal_details)
-    let users=[]
-     const promises =   personal_details.map((person, index) => {
-      const details = new PersonalDetails({ ...person, slot: sloted[index],slotDay6:slotedDay6[index] });
-     details.save().then(async saved=>{
-       users.push(saved._id)
-     let res=await TimeSlots.findOneAndUpdate(
-        { _id: sloted[index] },
-        { booked: true, user: saved._id,UserId:req.body.userId, ...req.body,packageid:packageid },
-        { new: true }
-      );
-      let res2=await TimeSlots.findOneAndUpdate(
-        { _id: slotedDay6[index] },
-        { booked: true, user: saved._id,UserId:req.body.userId, ...req.body,packageid:packageid },
-        { new: true }
-      );
-     // console.log(res)
-     return saved._id
-    })
+    console.log("here", personal_details);
+    let users = [];
+    const promises = personal_details.map((person, index) => {
+      const details = new PersonalDetails({
+        ...person,
+        slot: sloted[index],
+        slotDay6: slotedDay6[index],
+      });
+      details.save().then(async (saved) => {
+        users.push(saved._id);
+        let res = await TimeSlots.findOneAndUpdate(
+          { _id: sloted[index] },
+          {
+            booked: true,
+            user: saved._id,
+            UserId: req.body.userId,
+            ...req.body,
+            packageid: packageid,
+          },
+          { new: true }
+        );
+        let res2 = await TimeSlots.findOneAndUpdate(
+          { _id: slotedDay6[index] },
+          {
+            booked: true,
+            user: saved._id,
+            UserId: req.body.userId,
+            ...req.body,
+            packageid: packageid,
+          },
+          { new: true }
+        );
+        // console.log(res)
+        return saved._id;
+      });
     });
- 
 
     setTimeout(() => {
-  
-      res
-        .json({ message: "Booking Saved!",users:users , status: true });
- 
+      res.json({ message: "Booking Saved!", users: users, status: true });
     }, 1000);
-
-   
   } catch (error) {
     res.status(503).json({
       status: false,
@@ -327,8 +380,8 @@ exports.getPageById = (req, res) => {
   //console.log(req.body);
   Page.findById(req.params.id)
     .then((found) => {
-      console.log(found)
-      res.render('diphtheria',{ error: false, data: found });
+      console.log(found);
+      res.render("diphtheria", { error: false, data: found });
     })
     .catch((err) => {
       res.status(503).json({ error: true, msg: "Something Went Wrong" });
@@ -338,11 +391,14 @@ exports.getAlltestimonials = (req, res) => {
   //console.log(req.body);
   // categoryObject: { '$exists': false,}
 
-  testimonial.find({})
+  testimonial
+    .find({})
     .then((found) => {
-      res
-        .render('testimonials',{ error: false, data: found, 
-          PageCount: found.length });
+      res.render("testimonials", {
+        error: false,
+        data: found,
+        PageCount: found.length,
+      });
     })
     .catch((err) => {
       res
@@ -350,10 +406,12 @@ exports.getAlltestimonials = (req, res) => {
         .json({ error: true, msg: "Something Went Wrong", errMsg: err });
     });
 };
-exports.cancelMySlot=async(req,res)=>{
-  let response=await TimeSlots.findByIdAndUpdate(req.body.id,
-    { booked: false, user: null, UserId:null },
-  );
-  console.log(req.body.id)
-  res.json('Success')
-}
+exports.cancelMySlot = async (req, res) => {
+  let response = await TimeSlots.findByIdAndUpdate(req.body.id, {
+    booked: false,
+    user: null,
+    UserId: null,
+  });
+  console.log(req.body.id);
+  res.json("Success");
+};
