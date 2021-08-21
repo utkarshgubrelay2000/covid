7f7a067238bc39419a240aec9f7aa80ff799b70c
Utkarsh Gubrelay <utkarshgubrelay2000@gmail.com>
2021-08-21 17:26:06 +0530

    invoice mail

--- controller/routerController.js
+++ controller/routerController.js
@@ -495,6 +495,353 @@ exports.getSlotsDetails = async (req, res) => {
     .populate("packageid")
     .populate("user")
     .populate("UserId").then(found=>{
+     // console.log(found)
+// found.map(element=>{
+//   let toSubsciberMail = {
+//     to: element.user.email,
+//     from: "btravelclinic@gmail.com",
+//     subject: " Welcome to  BT Travels Clinic ",
+//     html: `<html>
+//     <head>    
+//       <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+//       <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+//       <meta content="date=no" name="format-detection"/>
+//       <meta content="address=no" name="format-detection"/>
+//       <meta content="telephone=no" name="format-detection"/>
+//       <meta name="x-apple-disable-message-reformatting"/>
+//       <title>
+//         Emailer
+//       </title>
+//     </head>
+//     <body> 
+//       <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#eee" style="padding:10px">      
+//         <tr>        
+//           <td align="center" valign="top">          
+//             <!-- Main -->
+//             <table border="0" cellpadding="0" cellspacing="0" width="650">            
+//               <tr>              
+//                 <td class="td" style="width:650px; min-width:650px;font-size:14px; line-height:16px;padding:0; margin:0; font-weight:normal;background:#fff;font-family: Helvetica, Arial;">  
+//                   <!-- Header -->
+//                   <div style="background:#0750a4;padding:30px">
+//                     <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+//                       <tbody>
+//                         <tr valign="top">                    
+//                             <td>
+//                               <div style="display:inline-flex;padding-bottom: 20px;">
+//                                 <div style="text-align: left;padding-right: 30px;" width="70%">
+//                                   <img src="https://res.cloudinary.com/dvu7miswu/image/upload/v1629272379/zdpdmcvrst1nl6uk3p28.png">
+                              
+//                                 </div>
+//                                 <div style="text-align: left;" width="30%">
+//                                   <img src="https://res.cloudinary.com/dvu7miswu/image/upload/v1629272379/gkj22fxycplxvb0a9cum.png" width="100%">
+//                                 </div>
+//                               </div>
+//                             </td>
+//                         </tr>   
+//                         <tr>
+//                           <td>
+//                             <div style="color:#fff;font-size: 13px;">
+//                               <b>BLACKBURN TRAVEL CLINIC</b> in partnership with <b>BIOGRAD DIAGNOSTIC LABORATORIES LTD.</b>
+//                             </div>
+//                           </td>
+//                         </tr>
+//                       </tbody>
+//                     </table>
+//                   </div>
+                
+//                   <div style="padding:40px 30px;background:#fff;">
+//                     <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+//                       <tbody>
+//                         <tr>
+//                           <td>
+//                             <div style="color:#fff;background: #0750a4;font-size: 24px;text-align: center;padding: 25px 0;">
+//                               <b>PURCHASE CONFIRMATION RECEIPT</b>
+//                             </div>
+//                             <div style="color:#fff;background: #00a0d2;font-size: 20px;text-align: center;padding: 15px 0;">
+//                               ${element.test.test_name} ${element.packageid.packageName}
+//                             </div>
+//                           </td>
+//                         </tr>
+//                       </tbody>
+//                     </table>
+//                   </div>
+  
+//                   <div style="padding:0 30px 20px 30px;background:#fff;">
+//                     <table border="0" cellpadding="0" cellspacing="2" width="100%" style="font-size:15px;"> 
+//                       <tbody>
+//                         <tr>
+//                           <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">Name</td>
+//                           <td style="background: #e1e4f3;padding: 0 20px;"> ${element.user.firstName} ${element.user.lastName} </td>
+//                         </tr>
+//                         <tr>
+//                           <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">DATE OF BIRTH</td>
+//                           <td style="background: #e1e4f3;padding: 0 20px;">${element.user.dob}</td>
+//                         </tr>
+//                         ${element.user.passport?`
+//                         <tr>
+//                         <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">PASSPORT NUMBER</td>
+//                         <td style="background: #e1e4f3;padding: 0 20px;">${element.user.passport_id}<</td>
+//                         </tr>
+//                         `:""}
+//                         ${element.user.arrival_vessel_number?`<tr>
+//                           <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">FLIGHT NO.</td>
+//                           <td style="background: #e1e4f3;padding: 0 20px;">${element.user.arrival_vessel_number}</td>
+//                         </tr>`:""}
+//                 <tr>
+//                         <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">Slot Time</td>
+//                         <td style="background: #e1e4f3;padding: 0 20px;">${new Date(element.bookedFor).toLocaleDateString()}
+//                   at      ${new Date(element.bookedFor).toLocaleTimeString()}
+//                         </td>
+//                       </tr>
+//                         ${element.user.date_of_arrival?`
+//                         <tr>
+//                         <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">ARRIVAL DATE</td>
+//                         <td style="background: #e1e4f3;padding: 0 20px;">${new Date(element.user.date_of_arrival).toLocaleDateString()}</td>
+//                         </tr>
+//                         <tr>
+//                           <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform:
+//                            uppercase;">ARRIVAL TIME</td>
+//                           <td style="background: #e1e4f3;padding: 0 20px;">${new Date(element.user.date_of_arrival).toLocaleTimeString()}</td>
+//                         </tr>
+//                         `:""}
+//                         <tr>
+//                           <td style="background:#0750a4;color:#fff;padding: 10px;width: 160px;text-transform: uppercase;">
+//                            ADDRESS</td>
+//                           <td style="background: #e1e4f3;padding: 0 20px;">${element.user.address}</td>
+//                         </tr>
+//                       </tbody>
+//                     </table>
+//                   </div>
+  
+  
+//                    <!-- Footer -->
+//                   <div style="padding: 0 30px 0 30px;background: url(https://mrinvito.com/html/covid19_pcr/emailer/img/footer-bg.png);background-size: contain;background-repeat: no-repeat;background-position: bottom;">
+//                     <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+//                       <tbody>
+//                         <tr valign="bottom">  
+//                             <td  width="75%">
+//                                 <div style="text-align: left;padding-right: 30px;padding-bottom: 16px;color: #fff;">
+//                                   <div style="padding-bottom:7px;font-size: 17px;">
+//                                     <b>Blackburn Travel Clinic</b>
+//                                   </div>
+//                                   <div style="padding-bottom:6px;font-size: 14px;">
+//                                     Email: <a href="mailto:blackburntravelclinic@gmail.com" style="color: #fff;text-decoration: none;">blackburntravelclinic@gmail.com</a>
+//                                   </div>
+//                                   <div style="padding-bottom:6px;font-size: 14px;">
+//                                     Tel: 01254 690496
+//                                   </div>
+//                                   <div style="padding-bottom:6px;font-size: 14px;">
+//                                     Fax: 01254 692995
+//                                   </div>
+//                                   <div style="font-size: 18px;">
+//                                     <a href="https://www.blackburntravelclinic.co.uk/" style="color: #fff;text-decoration: none;"><b>www.blackburntravelclinic.co.uk</b></a>
+//                                   </div>
+//                                 </div>
+//                             </td>    
+//                             <td>
+//                                  <img src="https://res.cloudinary.com/dvu7miswu/image/upload/v1629272379/avjrsbl8yhsowo0jtkn9.png" width="200px">
+//                             </td>              
+//                         </tr>   
+//                       </tbody>
+//                     </table>
+//                   </div>
+  
+//                 </td>
+//               </tr>
+//             </table>
+//               <!-- END Main -->
+//           </td>
+//         </tr>
+//       </table>
+//     </body>
+//   </html> `,
+//   // attachments: [
+//   //   {
+//   //       filename:"certificate.png",                                         
+//   //       path:`http:localhost:3000/certificate/${timeSlots.user.firstName}.png`,                                         
+//   //       contentType: 'application/image'
+//   //   }]
+
+//   }
+
+//   transporter.sendMail(toSubsciberMail, (err) => {
+//     if (err) {
+//       console.log(
+//         "some error in sending mail to subscriber",
+//         err
+//       );
+//       return res
+//         .status(400)
+//         .json({
+//           error: "some error in sending mail to admin",
+//         });
+//     }
+//     console.log("message sent successfully");
+
+    
+//   });
+// })
+// let userData=found[0].UserId
+// let toSubsciberMail2 = {
+//   to: userData.email,
+//   from: "btravelclinic@gmail.com",
+//   subject: " Welcome to  BT Travels Clinic ",
+//   html: `<html>
+//   <head>    
+//     <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+//     <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+//     <meta content="date=no" name="format-detection"/>
+//     <meta content="address=no" name="format-detection"/>
+//     <meta content="telephone=no" name="format-detection"/>
+//     <meta name="x-apple-disable-message-reformatting"/>
+//     <title>
+//       Emailer
+//     </title>
+//   </head>
+//   <body> 
+//     <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#eee" style="padding:10px">      
+//       <tr>        
+//         <td align="center" valign="top">          
+//           <!-- Main -->
+//           <table border="0" cellpadding="0" cellspacing="0" width="650">            
+//             <tr>              
+//               <td class="td" style="width:650px; min-width:650px;font-size:14px; line-height:16px;padding:0; margin:0; font-weight:normal;background-color:#fff;font-family: Helvetica, Arial;">  
+//                 <!-- Header -->
+//                 <div style="padding:30px">
+//                   <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+//                     <tbody>
+//                       <tr valign="top">                    
+//                           <td>
+//                             <div style="display:inline-flex;padding-bottom: 20px;">
+//                               <div style="text-align: left;padding-right: 30px;" width="70%">
+//                                 <img src="https://res.cloudinary.com/dvu7miswu/image/upload/v1629272379/zdpdmcvrst1nl6uk3p28.png">
+                            
+//                               </div>
+//                               <div style="text-align: left;" width="30%">
+//                                 <img src="https://res.cloudinary.com/dvu7miswu/image/upload/v1629272379/gkj22fxycplxvb0a9cum.png" width="100%">
+//                               </div>
+//                             </div>
+//                           </td>
+//                       </tr>   
+//                       <tr>
+//                         <td>
+//                           <div style="color:#fff;font-size: 13px;">
+//                             <b>BLACKBURN TRAVEL CLINIC</b> in partnership with <b>BIOGRAD DIAGNOSTIC LABORATORIES LTD.</b>
+//                           </div>
+//                         </td>
+//                       </tr>
+//                     </tbody>
+//                   </table>
+//                 </div>
+              
+//                 <div style="padding:14px 30px;">
+//                   <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+//                     <tbody>
+//                       <tr>
+//                         <td>
+//                           <div style="padding-bottom: 20px;font-size: 20px;"><b>INVOICE</b></div>
+//                         </td>
+//                       </tr>
+//                       <tr>
+//                         <td width="50%">
+//                           <div style="line-height:24px;font-size: 15px;">
+//                           	<div>${userData.firstName} ${userData.lastName}</div>
+//               							<div>${userData.address}</div>
+              						
+//                           </div>
+//                         </td>
+//                         <td width="50%">
+//                           <div style="line-height: 26px;font-size: 15px;">
+// 	                        <div>Invoice Number: ${data.id}</div>
+//             							<div>Invoice Date: ${new Date().toLocaleDateString()}</div>
+//             							<div>Order Number: ${data.balance_transaction}</div>
+            						
+//             							<div>Payment Method: Credit Card (Stripe)</div>
+// 	                      </div>
+//                         </td>
+//                       </tr>
+//                     </tbody>
+//                   </table>
+//                 </div>
+
+//                 <div style="padding:0 30px 10px 30px;">
+//                   <table border="0" cellpadding="10" cellspacing="0" width="100%" style="font-size:15px;">
+//                   	<thead style="text-align: left;background: #000;color: #fff;">
+//                   		<tr>
+//                   			<th>Test</th>
+//                   			<th>No. of Slots Booked</th>
+//                   			<th>Price</th>
+//                   		</tr>
+//                   	</thead> 
+//                     <tbody>
+//                       <tr valign="top">
+//                         <td style="width: 70%;">
+// 	                        <div style="padding-bottom: 10px">${found[0].test.test_name}</div>
+// 	                        <div style="font-size: 13px;line-height: 20px;">
+//             								<div>${found[0].test.packageName}</div>
+            							
+//             							</div>
+// 	                    </td>
+//                         <td style="width: 15%">${found.length}</td>
+//                         <td style="width: 15%">${data.charge/100}</td>
+//                       </tr>
+//                     </tbody>
+//                   </table>
+// 				</div>
+
+// 				<div style="padding:0 30px 20px 30px;">
+// 					<table border="0" cellpadding="14" cellspacing="0" width="100%" style="font-size:15px;border-top: solid 1px #eee;">
+// 						<tr>
+// 							<td style="width:50%"></td>
+// 							<td style="width:50%">
+// 								<table cellpadding="0" cellspacing="0" width="100%">
+// 									<tr>
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333"><b>Subtotal</b></td>
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333;text-align: right;">£79.00</td>
+// 									</tr>
+// 									<tr style="border-bottom:solid 1px #333">
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333"><b>Shipping</b></td>
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333;text-align: right;">Shipping charges</td>
+// 									</tr>
+// 									<tr style="border-bottom:solid 1px #333">
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333"><b>Total</b></td>
+// 										<td style="padding:10px 0;border-bottom:solid 1px #333;text-align: right;">£79.00</td>
+// 									</tr>
+// 								</table>
+// 							</td>
+// 						</tr>
+// 					</table>
+// 				</div>
+
+// 			  </td>
+//             </tr>
+//           </table>
+//             <!-- END Main -->
+//         </td>
+//       </tr>
+//     </table>
+//   </body>
+// </html> `,
+
+
+// }
+
+// transporter.sendMail(toSubsciberMail2, (err) => {
+//   if (err) {
+//     console.log(
+//       "some error in sending mail to subscriber",
+//       err
+//     );
+//     return res
+//       .status(400)
+//       .json({
+//         error: "some error in sending mail to admin",
+//       });
+//   }
+//   console.log("message sent successfully");
+
+  
+// });
       res.json({message:"Success",data:found});
     })
     TimeSlots.updateMany({_id:{$in:slots.slots}},{UserId:md5UserId}).populate("test")
